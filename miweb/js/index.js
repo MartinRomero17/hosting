bffe5d343d5918c69cb7db9c74b1ba6a6d3625d8
MartinRomero17 <67390618+MartinRomero17@users.noreply.github.com>
2023-02-20 01:48:23 -0300

    Add files via upload

---
+++ miweb/js/index.js
@@ -0,0 +1,14 @@
+var id = document.getElementById('btn')
+var btn = false
+
+function cambio() {
+    if(btn == false){
+        btn = true;
+    } else {
+        id.
+        btn = false;
+    }
+}
+
+
+btn.addEventListener('click',cambio,true)
